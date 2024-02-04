9066e85ee456aef416fd3272647f3818bffd7abf
Jose-Murilo <murilojucas360@hotmail.com>
2024-02-04 13:36:57 -0300

    feat: add project base with Elysia js and bunjs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from '@elysiajs/swagger'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia")
+
+app
+  .use(swagger())
+  .onError(({ code }) => {
+    if (code === 'NOT_FOUND') return 'Route not found :('
+  })
+  .post('/hello/:id', ({ params: { id }, body }) => {
+    const { name } = body
+    console.log(name)
+    // return name
+  }, {
+    params: t.Object({
+      id: t.Numeric()
+    }),
+    body: t.Object({
+      name: t.String()
+    })
+  })
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+
+app.handle(new Request('http://localhost/hello/3', {
+  method: 'POST',
+  body: JSON.stringify({
+    name: "Murilo"
+  })
+})).then(console.log)
+
+app.listen(3000);
